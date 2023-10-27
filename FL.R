b243c946c69c0f30c7072103a1754b8daf70c386
FilipLangiewicz <flangiewicz@gmail.com>
2023-10-27 19:21:53 +0200

    data McDonalds

--- FL.R
+++ FL.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(tidyr)
+library(ggplot2)
 
 
 burger_king_menu <- read.csv("data/burger-king-menu.csv")
@@ -7,3 +8,16 @@ deaths_obesity <- read.csv("data/deaths-due-to-obesity.csv")
 quick_service_restaurants_us <- read.csv("data/number-of-quick-service-restaurants-in-the-us-2011_2022.csv")
 Nutrition_Value_Dataset <- read.csv("data/Nutrition_Value_Dataset.csv")
 obesity <- read.csv("data/obesity-percents.csv")
+mc_in_Europe <- read.csv("data/mcdonalds_in_europe.csv", sep = ";")
+McZestaw <- read.csv("data/McZestawBigMac.csv")
+
+
+obesity %>% 
+  filter(Country == "United States of America",
+         Sex == "Both sexes") %>% 
+  ggplot(aes(x = Year, y = Obesity_percent, color = Sex)) +
+  geom_point()
+
+quick_service_restaurants_us %>% 
+  ggplot(aes(x = year, y = number_of_restaurants)) +
+  geom_point()
