7323667c50fb7b667c5cc8506078070ab32adafb
MaciejMomot <maciejmomot5@gmail.com>
2023-10-31 17:06:42 +0100

    Aktualizacja moich działań

--- MM.R
+++ MM.R
@@ -7,20 +7,50 @@ deaths_obesity <- read.csv("data/deaths-due-to-obesity.csv")
 quick_service_restaurants_us <- read.csv("data/number-of-quick-service-restaurants-in-the-us-2011_2022.csv")
 Nutrition_Value_Dataset <- read.csv("data/Nutrition_Value_Dataset.csv")
 obesity <- read.csv("data/obesity-percents.csv")
-number_of_fastfood_restaurants_worldwide <- read.csv("data/Number_of_fastfood_restaurants_worldwide.csv", sep = ";")
-deaths_obesity %>% 
-  filter(Entity == "United States") %>% 
-  ggplot(aes(x=Year, y = Deaths)) +
-  geom_point()
-colnames(deaths_obesity)[1] <- "Country"
-deaths_obesity %>% 
-  inner_join(obesity, by=(c("Country","Year")))
+dane <- read.csv("data/Number_of_fastfood_restaurants_worldwide.csv", sep = ";")
 
-
-# library(reshape2)
-# melted_data <- melt(dane, id.vars = "Year")
-# print(melted_data)
-# str(dane)
+library(reshape2)
+melted_data <- melt(dane, id.vars = "Year")
+print(melted_data)
+str(dane)
 selected_columns <- c("Domino.s.Pizza","KFC","Burger.King","PizzaHut","Subway","McDonald.s")
-number_of_fastfood_restaurants_worldwide[selected_columns] <- apply(number_of_fastfood_restaurants_worldwide[selected_columns], 2, function(x) as.numeric(gsub(" ", "", x)))
-str(number_of_fastfood_restaurants_worldwide)
+dane[selected_columns] <- apply(dane[selected_columns], 2, function(x) as.numeric(gsub(" ", "", x)))
+dane %>% 
+  na.omit() -> data_good
+library(reshape2)
+melted_data_good <- melt(data_good, id.vars = "Year")
+
+
+melted_data_good %>% 
+  mutate(logo = 0) -> melted_data_good_with_logo
+melted_data_good_with_logo$logo <- as.character(melted_data_good_with_logo$logo)
+
+melted_data_good_with_logo[c(19:27),4] <- paste("Loga", "logo3.png", sep = "/")
+melted_data_good_with_logo[c(10:18),4] <- paste("Loga", "logo2.png", sep = "/")
+melted_data_good_with_logo[c(1:9),4] <- paste("Loga", "logo1.png", sep = "/")
+melted_data_good_with_logo[c(28:36),4] <- paste("Loga", "logo4.png", sep = "/")
+melted_data_good_with_logo[c(37:45),4] <- paste("Loga", "logo5.png", sep = "/")
+melted_data_good_with_logo[c(46:54),4] <- paste("Loga", "logo6.png", sep = "/")
+
+
+library("ggimage")
+
+melted_data_good_with_logo %>% ggplot(aes(x=Year, y=value)) +
+  geom_image(aes(image=logo),size = .05) -> plot_with_logo
+
+
+melted_data_good_with_logo %>% ggplot(aes(x=Year, y=value, image=logo)) +
+  geom_line() + geom_point() + 
+  geom_image(size = .05) +
+  scale_x_continuous(breaks = seq(min(melted_data_good_with_logo$Year), max(melted_data_good_with_logo$Year), by = 1)) -> plot_with_logo_v2
+
+
+geom_line() + geom_point() + scale_x_continuous(breaks = seq(min(melted_data_good_with_logo$Year), max(melted_data_good_with_logo$Year), by = 1)) + 
+  scale_color_discrete(name = "Legenda") +
+  theme(legend.position="right")
+
+
+
+
+
+  
