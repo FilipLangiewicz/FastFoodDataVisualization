7fda7dcf8621d2967055219afa00eaca3904825d
FilipLangiewicz <flangiewicz@gmail.com>
2023-11-03 15:21:25 +0100

    barplot

--- FL.R
+++ FL.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(stringr)
+library(forcats)
 
 
 burger_king_menu <- read.csv("data/burger-king-menu.csv")
@@ -64,35 +65,68 @@ deaths_obesity %>%
   ggplot(aes(x = Year, y = dead)) + 
   geom_point()
 
-# choosing colors
+# barplot
+
+
+dietary_habits <- read.csv("data/Nutrition__Physical_Activity__and_Obesity.csv")
+
+df <- dietary_habits %>% 
+  group_by(Question) %>% 
+  distinct(Question)
+
+population <- read.csv("data/world_population.csv")
+
+types_of_mortality_vs_fried_food_consumption_frequency <- read.csv("data/Types of Mortality vs. Fried Food consumption Frequency-mean.csv")
+fried_food_consumption_and_mortality <- read.csv("data/Fried food consumption and mortality_ prospective cohort study.csv")
+
+frequency_of_visiting_fast_food <- read.csv("data/average-fast-food-consumption-per-week-in-2016-2018.csv", sep = ";")
+
+frequency_of_visiting_fast_food_modified <- frequency_of_visiting_fast_food %>% 
+  mutate(X2016 = X2016/100, X2017 = X2017/100, X2018 = X2018/100) %>% 
+  pivot_longer(cols = c(X2016, X2017, X2018), 
+               names_to = "Year", 
+               values_to = "PercentageShare") %>%
+  mutate(Year = as.factor(gsub("X", "", Year)))
+
 
 frequency_of_visiting_fast_food_modified %>% 
-  # filter(Year == 2018) %>% 
+  filter(Year == 2018) %>%
+  filter(Answer != "Prefer not to say") %>% 
+  mutate(pozycja = case_when(Answer == "I don't eat at fast food restaurants" ~ "A",
+                             Answer == "Less than once per week" ~ "B",
+                             Answer == "One to three times per week" ~ "c",
+                             Answer == "Four to six times per week" ~ "D",
+                             Answer == "Seven to nine times per week" ~ "E",
+                             Answer == "Ten times or more per week" ~ "F",
+                             TRUE ~ "Z")) %>% 
+  mutate(Answer = fct_reorder(Answer, pozycja)) %>% 
   ggplot(aes(y = Answer, x = PercentageShare, fill = Year)) +
   geom_bar(stat = "identity", position = position_dodge2(width = 0.5, preserve = "single"), width = 0.5) +
   labs(title = "How many times a week do you eat fast food?", 
        x = "Share of respondents (%)",
-       y = element_blank()) +
+       y = element_blank(),
+       legend = element_blank()) +
   theme_minimal() +
   theme(plot.background = element_rect(fill = '#18191C'),
         panel.grid.major = element_blank(),
         title = element_text(colour = "white",
                              family = "mono"),
         axis.text = element_text(colour = "white",
-                                 family = "mono"),
-        legend.text = element_text(colour = "white",
-                                   family = "mono")) +
-  scale_fill_manual(values = c("#E4D00A",
-                               "#0a1ee4",
-                               "#d00ae4")) +
-  scale_y_discrete(labels = c("4 - 6",
-                              "0",
-                              "< 1",
-                              "1 - 3",
-                              "?",
-                              "7 - 9",
-                              "> 9")) +
-  geom_vline(xintercept = 0, color = "white") -> plott
+                                 family = "mono",
+                                 size = 15,
+                                 face = "bold"),
+        panel.grid.major.x = element_line(),
+        
+        legend.position = "none") +
+  scale_fill_manual(values = c("#E4D00A")) +
+  scale_x_continuous(expand = expansion(c(0,0), c(0, 3)))
+x# scale_y_discrete(labels = c("4 - 6",
+  #                             "0",
+  #                             "< 1",
+  #                             "1 - 3",
+  #                             "7 - 9",
+  #                             "> 9")) +
+   
 
 
     
